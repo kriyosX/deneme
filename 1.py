5c58cea065e15e34c28a4c1641c8581094831e70
kriyosX <34481937+kriyosX@users.noreply.github.com>
2018-03-18 17:17:18 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ import socket
 hedef_sunucu = "www.facebook.com"
 hedef_port = 443
 
-sunucu = socket.socket(AF_INET , socket.SOCK_STREAM)
+sunucu = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 
 sunucu.connect((hedef_sunucu , hedef_port))
 
